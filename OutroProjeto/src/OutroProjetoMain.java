04774aca2934b8569a9438658cb97a4799a27e55
AntonioHReis <ahreis@gmail.com>
2021-02-06 15:04:40 -0300

    exercício de aula, alteração do código

--- OutroProjeto/src/OutroProjetoMain.java
+++ OutroProjeto/src/OutroProjetoMain.java
@@ -3,5 +3,6 @@ public class OutroProjetoMain {
     public static void main(String[] args) {
         System.out.println("Outro projeto main");
         System.out.println("Fiz outra linha de comando, vamos ver o que vai acontecer no Git");
+        System.out.println("Outra mensagem aparecendo na tela, para submeter ao github");
     }
 }
