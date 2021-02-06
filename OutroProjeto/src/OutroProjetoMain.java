2beb68e34162bb75cbf8334dea893ac2a7aa0fb6
AntonioHReis <ahreis@gmail.com>
2021-02-06 14:41:14 -0300

    verificando alterações

--- OutroProjeto/src/OutroProjetoMain.java
+++ OutroProjeto/src/OutroProjetoMain.java
@@ -2,5 +2,6 @@ public class OutroProjetoMain {
 
     public static void main(String[] args) {
         System.out.println("Outro projeto main");
+        System.out.println("Fiz outra linha de comando, vamos ver o que vai acontecer no Git");
     }
 }
